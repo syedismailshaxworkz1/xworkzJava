32897beec057311d2c465fd0afff65ce520230ce
syedismailshaxworkz1 <145985377+syedismailshaxworkz1@users.noreply.github.com>
2023-09-25 06:36:13 -0700

    Add files via upload

---
+++ Fst.java
@@ -0,0 +1,32 @@
+public class Bag 
+{
+   Bag(int number) {
+   System.out.println("it is my sbag");
+    }
+
+	Bag() {
+   System.out.println("it is my sbag");
+    }
+
+
+public static void main (String[] args)
+ {
+ new Bag(3);
+ new Bag(2);   
+ }
+ 
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
